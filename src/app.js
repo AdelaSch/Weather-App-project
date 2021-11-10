0f46b76236b732f84b1d87f13e6d05a0aeb59578
AdelaSch <adelschieblova@gmail.com>
2021-11-10 11:06:31 +0100

    Fixed Api URL

--- src/app.js
+++ src/app.js
@@ -9,6 +9,6 @@ function displayForecast(response) {
 }
 
 let apiKey = "82aab33db8911af682203374eb6fbc22";
-let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayForecast);
