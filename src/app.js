79432056656ea498380be1ef1ec6e915217f0aca
AdelaSch <adelschieblova@gmail.com>
2021-11-13 19:16:42 +0100

    Updated app.js
    linked API to display forecast

--- src/app.js
+++ src/app.js
@@ -22,7 +22,8 @@ function formatDate(timezone) {
   return `${day}, ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastDays = ["Thu", "Fri", "Sat", "Sun"];
@@ -48,7 +49,11 @@ function displayForecast() {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
-
+function getForecast(coordinates) {
+  let apiKey = "82aab33db8911af682203374eb6fbc22";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unit=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#current-temp");
   let cityElement = document.querySelector("#current-city");
@@ -63,6 +68,8 @@ function displayTemperature(response) {
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   iconElement.setAttribute("src", `icons/${response.data.weather[0].icon}.png`);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -106,4 +113,3 @@ let celsiusLink = document.querySelector("#unit-celsius");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Tampa");
-displayForecast();
